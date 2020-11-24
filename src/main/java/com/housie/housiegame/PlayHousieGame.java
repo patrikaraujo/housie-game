a32b957b734703678ac42ee19d2f6e95aac1abc4
Patrik Araujo <patrikaraujo@gmail.com>
2020-11-23 22:24:06 -0800

    Housie Game
    - Changing the PlayHousieGame class to match the text of the
    requirements.
    - Refactoring the PlayHousieGame class, extracting a few methods out of
    the main method.

--- src/main/java/com/housie/housiegame/PlayHousieGame.java
+++ src/main/java/com/housie/housiegame/PlayHousieGame.java
@@ -9,6 +9,8 @@
 package com.housie.housiegame;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
@@ -53,29 +55,32 @@ public class PlayHousieGame {
 		}
 	}
 
-	public static void main(String[] args) {
+	private static GameInputs captureGameInputs() {
 		System.out.println("**** Let's Play Housie ****");
-		System.out.println("Note: - Press 'Q' to quit any time.");
+		System.out.println("Note: - Press 'Q' to quit any time.\n");
 		Scanner scanner = new Scanner(System.in);
 		GameInputs gameInputs = null;
 		while (true) {
-			System.out.print(">> Enter the number range (1-n): ");
-			int numbersRange = readInteger(scanner, "numbersRange");
+			System.out.print(">> Enter the number range (1-n): Default to 1-90: ");
+			int numbersRange = readInteger(scanner, 90, "numbersRange");
 			System.out.print(">> Enter number of players playing the game: ");
 			int players = readInteger(scanner, "players");
 			System.out.print(">> Enter ticket size: Default to 3x10: ");
 			String ticketSize = readTicketSize(scanner, "3x10");
-			System.out.print(">> Enter numbers per row. Default to 5: ");
+			System.out.print(">> Enter numbers per row: Default to 5: ");
 			int numbersPerRow = readInteger(scanner, 5, "numbersPerRow");
 			try {
 				gameInputs = new GameInputs(numbersRange, players, ticketSize, numbersPerRow);
 				break;
 			} catch (IllegalArgumentException e) {
-				System.out.println(e.getLocalizedMessage());
-				System.out.println("Review the inputs and try again.");
+				System.out.println(e.getLocalizedMessage() + "\n");
+				System.out.println("Review the inputs and try again.\n");
 			}
 		}
+		return gameInputs;
+	}
 
+	private static GameStatus drawGameNumbers(HousieGame game) {
 		Terminal terminal = null;
 		try {
 			terminal = TerminalBuilder.builder().jna(true).system(true).build();
@@ -83,14 +88,14 @@ public class PlayHousieGame {
 			e.printStackTrace();
 		}
 
-		HousieGame game = new HousieGame(gameInputs);
-
 		GameStatus status = game.getGameStatus();
 		terminal.enterRawMode();
 		NonBlockingReader reader = terminal.reader();
+		System.out.println("\n**** Tickets Created Successfully ****");
+		System.out.println(">> Press N to generate next number: ");
 		int valueRead = 0;
 		do {
-			System.out.print(">> Press N to generate next number: ");
+			System.out.print("Next number is: ");
 			try {
 				valueRead = reader.read();
 				if (valueRead == CHAR_N || valueRead == CHAR_UPPERCASE_N) {
@@ -98,7 +103,8 @@ public class PlayHousieGame {
 					System.out.println(status.getTurnNumberCalled());
 					if (status.hasTurnWinner()) {
 						for (Entry<Rules, Player> entry : status.getTurnWinners().entrySet()) {
-							System.out.println(entry.getValue().toString() + " has won " + entry.getKey().toString());
+							System.out.println("We have a winner: " + entry.getValue().toString() + " has won '"
+									+ entry.getKey().toString() + "' winning combination.");
 						}
 					}
 				} else {
@@ -108,11 +114,33 @@ public class PlayHousieGame {
 				e.printStackTrace();
 			}
 		} while (valueRead != CHAR_Q && valueRead != CHAR_UPPERCASE_Q && !status.isGameOver());
-		System.out.println("Game Summary");
-		for (Entry<Rules, Player> entry : status.getWinners().entrySet()) {
-			System.out.println(entry.getValue().toString() + " has won " + entry.getKey().toString());
-		}
+		return status;
+	}
 
+	public static void displayGameOver(GameStatus status) {
+		System.out.println("\n***** Game Over *****\n");
+		System.out.println("=====================");
+		System.out.println("       Summary");
+		Map<String, String> winnersMap = new HashMap<String, String>();
+		for (Entry<Rules, Player> entry : status.getWinners().entrySet()) {
+			String message = "";
+			if (winnersMap.containsKey(entry.getValue().toString()))
+				message += winnersMap.get(entry.getValue().toString()) + " and " + entry.getKey().toString();
+			else
+				message += entry.getKey().toString();
+			winnersMap.put(entry.getValue().toString(), message);
+		}
+		for (Entry<String, String> entry : winnersMap.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+		System.out.println("=====================");
+	}
+
+	public static void main(String[] args) {
+		GameInputs gameInputs = captureGameInputs();
+		HousieGame game = new HousieGame(gameInputs);
+		GameStatus status = drawGameNumbers(game);
+		displayGameOver(status);
 	}
 
 }
